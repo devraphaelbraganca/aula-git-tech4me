eaa0d48f1dea2319ccb14bd40c686d8c96689733
flexxphael <94576147+flexxphael@users.noreply.github.com>
2021-11-17 22:02:17 -0300

    Atualização para acelerar somente a velocidade for maior que a atual

--- direcao/Carro.java
+++ direcao/Carro.java
@@ -1,9 +1,19 @@
 package direcao;
+
 public class Carro {
     private int velocidade;
-    public void acelerar(int velocidade) {
-        this.velocidade = velocidade;
+
+    public int acelerar(int velocidade) {
+        int diferenca = 0;
+        
+        if (velocidade > this.velocidade) {
+            diferenca = velocidade - this.velocidade;
+            this.velocidade = velocidade;
+        }
+        
+        return diferenca;
     }
+
     public int getVelocidade() {
         return this.velocidade;
     }
